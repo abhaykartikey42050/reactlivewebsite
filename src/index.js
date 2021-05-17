92af1f80c8f756fd63e5666f1ba4e22a8ac8b620
Abhay Kartikey <abhaykartikey42050@gmail.com>
2021-05-17 15:45:27 +0530

    route not working when reload changes ddfd

--- src/index.js
+++ src/index.js
@@ -5,7 +5,7 @@ import App from './App';
 import {BrowserRouter} from 'react-router-dom';
 
 ReactDOM.render(
-  <BrowserRouter>
+  <BrowserRouter {process.env.PUBLIC_URL +'/'}>
     <App />
   </BrowserRouter>,
   document.getElementById('root')
